b127c477f9a229f91db9c8ad49e49753c189ee80
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2021-01-13 19:08:00 +0530

    Update contact7_download.php

--- plugin-customization/contact7_download.php
+++ plugin-customization/contact7_download.php
@@ -1,6 +1,6 @@
 <script>
 document.addEventListener( 'wpcf7mailsent', function( event ) {
-  if ( '8' == event.detail.contactFormId ) {
+  if ( '2485' == event.detail.contactFormId ) {
     location = 'https://www.ecloudcontrol.com/wp-content/uploads/2021/01/AppZ-Healthcare-Case-Study-Oct-2020-v2-1.pdf';
   }
 }, false );
