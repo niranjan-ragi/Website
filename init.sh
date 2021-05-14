fba1547180cdc768d8e2223ee221352d77aedba9
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2021-05-14 15:40:00 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -25,3 +25,16 @@ else
 fi
 rm error.txt
 fi
+filename2="CC_Brochure_May2021-1"
+if  grep $filename2  -q  /var/www/html/wp-content/themes/Avada/header.php ;then
+  echo "theme change already exist..."
+else
+    sed -n -i -e '/<?php wp_head(); ?>/r /wp-contents/plugin-customization/contact7_download_02.php' -e 1x -e '2,${x;p}' -e '${x;p}' /var/www/html/wp-content/themes/Avada/header.php 2>error.txt
+    if [ -s error.txt ];then
+            rm error.txt
+            echo "failed to update header.php"
+    else
+        echo "header.php successfully updated"
+fi
+rm error.txt
+fi
