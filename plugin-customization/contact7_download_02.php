9d8064ba11388939c2f9370017c94b585059afad
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2021-05-14 15:38:06 +0530

    Create contact7_download_02.php

---
+++ plugin-customization/contact7_download_02.php
@@ -0,0 +1,7 @@
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+  if ( '3342' == event.detail.contactFormId ) {
+    location = 'https://www.ecloudcontrol.com/wp-content/uploads/2021/05/CC_Brochure_May2021-1.pdf';
+  }
+}, false );
+</script>
