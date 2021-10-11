55412d3c0c57012971445a932b02bfb76c70937a
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2021-10-11 16:01:14 +0530

    Create contact7_download_03.php

---
+++ plugin-customization/contact7_download_03.php
@@ -0,0 +1,7 @@
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+  if ( '4186' == event.detail.contactFormId ) {
+    location = 'https://www.ecloudcontrol.com/wp-content/uploads/2021/10/AppZ-CloudControl-brochure.pdf';
+  }
+}, false );
+</script>
