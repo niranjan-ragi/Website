653c755bf1d6e37240ec5b19c42b30d489169d6d
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2022-05-18 12:22:15 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -52,3 +52,17 @@ else
 fi
 rm error.txt
 fi
+
+filename4="EdTech Startup Case Study  Migrating Their Application  To Kubernetes On Cloud"
+if  grep $filename4  -q  /var/www/html/wp-content/themes/Avada/header.php ;then
+  echo "theme change already exist..."
+else
+    sed -n -i -e '/<?php wp_head(); ?>/r /wp-contents/plugin-customization/contact7_download_04.php' -e 1x -e '2,${x;p}' -e '${x;p}' /var/www/html/wp-content/themes/Avada/header.php 2>error.txt
+    if [ -s error.txt ];then
+            rm error.txt
+            echo "failed to update header.php"
+    else
+        echo "header.php successfully updated"
+fi
+rm error.txt
+fi
