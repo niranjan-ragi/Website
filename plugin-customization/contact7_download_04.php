77df26ebfc11b4660a4f1a8720ead4e999af21bf
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2022-05-19 12:01:07 +0530

    Update contact7_download_04.php

--- plugin-customization/contact7_download_04.php
+++ plugin-customization/contact7_download_04.php
@@ -1,6 +1,6 @@
 <script>
 document.addEventListener( 'wpcf7mailsent', function( event ) {
-  if ( '3342' == event.detail.contactFormId ) {
+  if ( '4732' == event.detail.contactFormId ) {
     location = 'https://www.ecloudcontrol.com/wp-content/uploads/2022/05/Case-Study-EdTech-Cloud-Native-v05.pdf';
   }
 }, false );
