370d7eb4b82642303ee845cc04290d661cdf8b9b
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2022-06-15 12:13:47 +0530

    Create contact7_download_05.php

---
+++ plugin-customization/contact7_download_05.php
@@ -0,0 +1,7 @@
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+  if ( '4768' == event.detail.contactFormId ) {
+    location = 'https://www.ecloudcontrol.com/wp-content/uploads/2022/06/CCS-AppZ-for-AutoMobile-Company-V01.pdf';
+  }
+}, false );
+</script>
