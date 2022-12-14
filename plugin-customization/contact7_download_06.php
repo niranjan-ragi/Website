7fed16383c07c8dc2891bdba62501431d7cdf3ca
pradeepchandran0 <43405813+pradeepchandran0@users.noreply.github.com>
2022-12-14 19:20:33 +0530

    Create contact7_download_06.php

---
+++ plugin-customization/contact7_download_06.php
@@ -0,0 +1,7 @@
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+  if ( '5458' == event.detail.contactFormId ) {
+    location = 'https://www.ecloudcontrol.com/wp-content/uploads/2022/12/Signature-SRE-Dec2022-v8.pdf';
+  }
+}, false );
+</script>
